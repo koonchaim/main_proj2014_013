5c9b1c4267017089a520ea6d239b26abf79e4aa6
koonchaim <koonchaim@gmail.com>
2014-10-15 16:47:05 +0700

    Commit

--- src/MainProgram/LoginUseCase/RunLogin.java
+++ src/MainProgram/LoginUseCase/RunLogin.java
@@ -8,7 +8,7 @@ public class RunLogin {
 	public static void main(String[] args) {
 		String username = "admin";
 		String password = "123456";
-
+		//----
 		LoginManageMent loginMng = new LoginManageMent();
 		LoginBean loginBean = new LoginBean(username, password);
 		loginBean = loginMng.vertifyLogin(loginBean);
