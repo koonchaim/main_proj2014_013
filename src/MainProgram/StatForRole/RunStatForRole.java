53d8e5bda6f3af8f0c427d31b5b708e6a9745511
koonchaim <koonchaim@gmail.com>
2014-10-28 17:02:05 +0700

    Stat For Role UseCase

---
+++ src/MainProgram/StatForRole/RunStatForRole.java
@@ -0,0 +1,15 @@
+package MainProgram.StatForRole;
+
+public class RunStatForRole {
+
+	public static void main(String[] args) {
+		int presents = 1;
+
+		int lacks = 1;
+
+		int sickLeaves = 1;
+
+		int personalLeaves = 1;
+	}
+
+}
--- src/MainProgram/ViewProfileTeacherUseCase/RunViewProfileTeacher.java
+++ src/MainProgram/ViewProfileTeacherUseCase/RunViewProfileTeacher.java
@@ -1,11 +1,9 @@
 package MainProgram.ViewProfileTeacherUseCase;
 
-import MainProgram.ViewProfileTeacherUseCase.Bean.ViewProfileTeacherManageMent;
-
 public class RunViewProfileTeacher {
 
 	public static void main(String[] args) {
-		ViewProfileTeacherManageMent viewMsg = new ViewProfileTeacherManageMent();
+
 	}
 
 }
